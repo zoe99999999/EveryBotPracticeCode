29e836bf57e44175221851b502ff47181562f2ae
1023941311414 <jake.widmann@outlook.com>
2023-11-18 15:02:58 -0500

    started drive subsystem

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,9 +6,12 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.drive.DriveSubsystem;
 
 public class RobotContainer {
+  DriveSubsystem driveSubsystem = new DriveSubsystem();
   public RobotContainer() {
+    
     configureBindings();
   }
 
---
+++ src/main/java/frc/robot/drive/DriveSubsystem.java
@@ -0,0 +1,17 @@
+package frc.robot.drive;
+
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class DriveSubsystem extends SubsystemBase{
+    TalonSRX driveMoterL1 = new TalonSRX();
+    TalonSRX driveMoterL2 = new TalonSRX();
+    TalonSRX driveMoterR1 = new TalonSRX();
+    TalonSRX driveMoterR2 = new TalonSRX();
+
+    public DriveSubsystem (){
+
+    }
+    public void 
+}
