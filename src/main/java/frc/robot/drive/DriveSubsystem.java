ffa843af284ac8ce60b71b38c6f99bf63a842f43
renieharris <renieharris124@gmail.com>
2023-12-06 19:03:17 -0500

    fixed vedor dep things

--- src/main/java/frc/robot/drive/DriveSubsystem.java
+++ src/main/java/frc/robot/drive/DriveSubsystem.java
@@ -5,13 +5,12 @@ import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class DriveSubsystem extends SubsystemBase{
-    TalonSRX driveMoterL1 = new TalonSRX();
-    TalonSRX driveMoterL2 = new TalonSRX();
-    TalonSRX driveMoterR1 = new TalonSRX();
-    TalonSRX driveMoterR2 = new TalonSRX();
+    //TalonSRX driveMoterL1 = new TalonSRX();
+    //TalonSRX driveMoterL2 = new TalonSRX();
+    //TalonSRX driveMoterR1 = new TalonSRX();
+    //TalonSRX driveMoterR2 = new TalonSRX();
 
     public DriveSubsystem (){
 
     }
-    public void 
 }
